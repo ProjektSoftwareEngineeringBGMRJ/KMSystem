bf34d860e1844441013959bba6b88946bf7e829a
Jan Kappus <jmkr@gmx.net>
2025-11-01 01:41:00 +0100

    controller

--- controller.py
+++ controller.py
@@ -363,6 +363,9 @@ def benutzer_loeschen():
             if anzahl_admins <= 1:    
                 flash("Mindestens ein Admin muss erhalten bleiben.")
                 return redirect(url_for("nutzer_verwalten"))
+            else:
+                db.session.delete(benutzer)
+                db.session.commit()
         elif(benutzer.equals(current_user)):
             flash("Nicht erlaubt sich selber zu löschen!") 
         else:
